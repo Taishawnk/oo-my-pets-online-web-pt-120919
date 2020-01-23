dccb7bd089ad0ec00428625a5dc390733f4d5016
Taishawn King <taishawnking1@yahoo.com>
2020-01-23 01:15:01 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,38 @@
+require"pry"
 class Owner
-  # code goes here
+  attr_reader :species,:name 
+  @@all = []
+  def initialize(name)
+    @name= name
+    @species = "human"
+    @@all << self
+  end
+  def say_species
+  return "I am a #{@species}."
+  end
+  def self.all
+    return @@all 
+  end 
+  def self.count
+     self.all.length
+  end 
+  def self.reset_all
+  self.all.clear
+  end
+  def cats 
+  Cat.all.select {|cat| cat.owner == self}
+  end
+  def dogs 
+  Dog.all.select {|dog| dog.owner == self}
+  end
+  def buy_cat(name) 
+ Cat.new(name, self)
+  end 
+  def buy_dog(name)
+  Dog.new(name, self)
 end
+def walk_dogs
+dogs.each {|walk| walk.mood = "happy" }
+end
+end 
+
